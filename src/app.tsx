8be5e3d96259b5f147439cf4961518a190d60a9a
pontakornth <pontakorn61@gmail.com>
2020-11-09 15:36:02 +0700

    init: initial commit

---
+++ src/app.tsx
@@ -0,0 +1,21 @@
+import { h, Fragment, FunctionalComponent  } from 'preact'
+
+const App: FunctionalComponent = () => {
+  return (
+    <>
+      <p>Hello Vite + Preact!</p>
+      <p>
+        <a
+          class="link"
+          href="https://preactjs.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Preact
+        </a>
+      </p>
+    </>
+  )
+}
+
+export default App
